02eab7da4f93dc5379e4f24232c8d342ceadd1aa
sungmin <95615086+Clariatinet@users.noreply.github.com>
2025-12-14 15:31:39 +0900

    java-study 25.12.14

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1을 출력합니다.
+        //System.out.println("Hello Java2");
+
+        /*
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}
---
+++ src/variable/Var5.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        // 1. 변수 선언과 초기화를 각각 따로 진행
+        int a;
+        a = 10;
+        System.out.println(a);
+
+        // 2. 변수를 선언과 초기화를 한번에
+        int b = 10;
+        System.out.println(b);
+
+        // 3. 여러 변수를 선언과 초기화를 한번에
+        int c = 20, d = 30;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
