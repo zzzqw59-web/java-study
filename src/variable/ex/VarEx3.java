a0c42b3a47f3c2551667794c77a98a1b0d54b036
sungmin <95615086+Clariatinet@users.noreply.github.com>
2025-12-14 19:33:18 +0900

    java-study 산술 연산자

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        boolean b = true;
+
+        System.out.println(l);
+        System.out.println(b);
+
+
+    }
+}
