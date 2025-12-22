97594297fa311b5b9e4d90398053865368614769
sungmin <zzzqw59@gmail.com>
2025-12-22 23:30:54 +0900

    연산자 문제 풀이

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,10 @@
+package operator;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+
+        boolean result = score >= 80 && 100 >= score;
+        System.out.println(result);
+    }
+}
