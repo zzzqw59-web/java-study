68dced59fdaa4155610da16f0f0e264ab2dc83c5
sungmin <zzzqw59@gmail.com>
2025-12-29 20:26:45 +0900

    조건문 if, else if

---
+++ src/cond/If3.java
@@ -0,0 +1,27 @@
+package cond;
+
+public class If3 {
+    public static void main(String[] args) {
+        int age = 8;
+
+        if (age <= 7) {
+            System.out.println("미취학 아동입니다.");
+        }
+
+        if (age >= 8 && age <= 13) {
+            System.out.println("초등학생입니다.");
+        }
+
+        if (age >= 14 && age <= 16) {
+            System.out.println("중학생입니다.");
+        }
+
+        if (age >= 17 && age <= 19) {
+            System.out.println("고등학생입니다.");
+        }
+
+        if (age >= 20) {
+            System.out.println("성인입니다.");
+        }
+    }
+}
