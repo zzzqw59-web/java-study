565133174007404e279108bfa22efbfe9660c0ad
sungmin <zzzqw59@gmail.com>
2026-01-05 23:18:55 +0900

    조건문 while 1

---
+++ src/loop/While1_1.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class While1_1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count + 1;
+        System.out.println("현재 값은 :" + count);
+        count = count + 1;
+        System.out.println("현재 값은 :" + count);
+        count = count + 1;
+        System.out.println("현재 값은 :" + count);
+
+
+
+    }
+}
