7ef0682b7cf8d2a81e9d1aaf6751776c2fabc764
sungmin <zzzqw59@gmail.com>
2026-01-12 22:40:32 +0900

    형변환

---
+++ src/casting/Casting3.java
@@ -0,0 +1,17 @@
+package casting;
+
+public class Casting3 {
+    public static void main(String[] args) {
+        long maxIntValue = 2147483647; // int 최고값
+        long maxIntOver = 2147483648L; // int 최고값 + 1 => 초과
+        int intValue = 0;
+
+        intValue = (int) maxIntValue; //형변환
+        System.out.println("intValue = " + intValue);
+
+        maxIntOver = (int) intValue; //형변환
+        System.out.println("maxIntOver = " + maxIntOver);
+
+
+    }
+}
---
+++ src/casting/Casting4.java
@@ -0,0 +1,17 @@
+package casting;
+
+public class Casting4 {
+    public static void main(String[] args) {
+        int div1 = 3 / 2;
+        System.out.println(div1);
+
+        double div2 = 3 / 2;
+        System.out.println(div2);
+
+        double div3 = 3.0 / 2;
+        System.out.println(div3);
+
+        double div4 = (double) 3 / 2;
+        System.out.println(div4);
+    }
+}
