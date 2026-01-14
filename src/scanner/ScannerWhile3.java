55394337ffba039d9264e065729e2da8a01572c5
sungmin <zzzqw59@gmail.com>
2026-01-14 22:22:21 +0900

    스캐너 예제 1

---
+++ src/scanner/ScannerWhile3.java
@@ -0,0 +1,22 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            System.out.print("정수를 입력하세요, 0을 입력하면 종료.");
+            int number = input.nextInt();
+
+            if (number == 0) {
+                break;
+            }
+            sum += number;
+        }
+        System.out.print("모든 숫자의 합" + sum);
+    }
+}
---
+++ src/scanner/ex/ScannerEx1.java
@@ -0,0 +1,17 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("당신의 이름을 입력하세요:");
+        String name = input.nextLine();
+
+        System.out.print("당신의 나이를 입력하세요:");
+        int age = input.nextInt();
+
+        System.out.println("당신의 이름은 " + name + "이고, 당신의 나이는 " + age + "입니다.");
+    }
+}
