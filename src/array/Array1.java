0baf2ce1028a6f2465660c0b0cacbebdf5e492fb
sungmin <zzzqw59@gmail.com>
2026-01-18 11:46:55 +0900

    배열의 기초

---
+++ src/array/Array1.java
@@ -0,0 +1,23 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
+        int student6 = 40;
+        int student7 = 30;
+
+
+        System.out.println("학생 1의 점수:" + student1);
+        System.out.println("학생 2의 점수:" + student2);
+        System.out.println("학생 3의 점수:" + student3);
+        System.out.println("학생 4의 점수:" + student4);
+        System.out.println("학생 5의 점수:" + student5);
+        System.out.println("학생 6의 점수:" + student6);
+        System.out.println("학생 7의 점수:" + student7);
+
+    }
+}
